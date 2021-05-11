f803f4561a74b702c5be85addd4eac74669acbf3
Myron <myron890@gmail.com>
2021-05-11 11:41:10 +0200

    Print location when user long presses on Map

--- Virtual Tourist/Controller/TravelLocationsMapViewController.swift
+++ Virtual Tourist/Controller/TravelLocationsMapViewController.swift
@@ -17,7 +17,25 @@ class TravelLocationsMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Allow mapView to respond to user long taps
+        setupGestureRecognizerForMapView()
+
+    }
+}
+
+    //Add functionality for MapView to respond to user long press gestures
+extension TravelLocationsMapViewController {
+
+    func setupGestureRecognizerForMapView(){
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleLongPress(_:)))
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
 
-
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
+        let location = gestureRecognizer.location(in: mapView)
+        let coordinate  = mapView.convert(location, toCoordinateFrom: mapView)
+        print(coordinate)
+    }
+    
 }
