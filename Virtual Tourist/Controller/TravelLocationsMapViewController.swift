5fe71cb815e421829ba54a3cf9b776465aaf4dc6
Myron <myron890@gmail.com>
2021-05-11 13:23:30 +0200

    Add pin to map after long press gesture

--- Virtual Tourist/Controller/TravelLocationsMapViewController.swift
+++ Virtual Tourist/Controller/TravelLocationsMapViewController.swift
@@ -18,24 +18,30 @@ class TravelLocationsMapViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //Allow mapView to respond to user long taps
+        //Allow mapView to respond to user long taps to add a pin
         setupGestureRecognizerForMapView()
 
     }
 }
 
-    //Add functionality for MapView to respond to user long press gestures
+    //Add functionality for MapView to respond to user long press gestures to add a pin
 extension TravelLocationsMapViewController {
 
     func setupGestureRecognizerForMapView(){
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleLongPress(_:)))
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.handleLongPress(_:)))
         mapView.addGestureRecognizer(gestureRecognizer)
     }
 
     @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer) {
         let location = gestureRecognizer.location(in: mapView)
         let coordinate  = mapView.convert(location, toCoordinateFrom: mapView)
-        print(coordinate)
+        addPintoMapViewFrom(coordinate)
+    }
+    
+    func addPintoMapViewFrom(_ coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
     }
     
 }
